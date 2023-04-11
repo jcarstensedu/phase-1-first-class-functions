c9c1c53ee6300ec0ff4bfadd3718bc8c9f884f9f
johnathan carstens <jcarstensedu@gmail.com>
2023-04-11 10:58:33 -0600

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function receivesAFunction(spy) {
+    spy()
+}
+
+
+
+
+function returnsANamedFunction() {
+    return function anything() { }
+}
+
+
+
+function returnsAnAnonymousFunction() {
+    return function () { }
+}
+
